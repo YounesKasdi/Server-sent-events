1bd6530081181be463dfc2e050e013be19416d1f
Cryptic <37178173+younesKSD@users.noreply.github.com>
2018-07-05 13:08:55 +0100

    Update Transmit.js

--- js/Transmit.js
+++ js/Transmit.js
@@ -1,6 +1,4 @@
-$(document).ready(function(){
-  var evtSource = new EventSource('php/transmit.php');
-  evtSource.onmessage = function(event) {
-    document.getElementById("result").innerHTML += event.data + "<br>";
-  }
-});
+var evtSource = new EventSource('php/transmit.php');
+evtSource.onmessage = function(event) {
+  document.getElementById("result").innerHTML += event.data + "<br>";
+}
